50f36f67960bb75978dd79c5de14dcb81c946a1b
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-28 05:50:11 +0500

    compressed img, refactor folders structure in the app, refactor script(added intersectionObserver)

--- dist/script.js
+++ dist/script.js
@@ -1,32 +1,26 @@
 (function() {
-  const OFFSET_BEFORE_SECTION = 150;
-  const appSections = document.querySelectorAll("section");
+  document.addEventListener("DOMContentLoaded", function() {
+    var appSections = [].slice.call(
+      document.querySelectorAll("section")
+    );
 
-  document.addEventListener(
-    "DOMContentLoaded",
-    scrollSections(appSections)
-  );
-
-  function scrollSections(sections) {
-    if (window.innerWidth > 767) {
-      addingEventListenerToSections(sections);
-    } else {
-      for (const section of sections) {
-        section.classList.add("active");
-      }
-    }
-  }
-
-  function addingEventListenerToSections(sections) {
-    window.addEventListener("scroll", function() {
-      for (const section of sections) {
-        const sectionCoordinate = this.innerHeight + this.scrollY;
-        const scrollPage = section.offsetTop;
-
-        if (sectionCoordinate - OFFSET_BEFORE_SECTION > scrollPage) {
-          section.classList.add("active");
+    if ("IntersectionObserver" in window) {
+      let sectionsObserver = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(({ isIntersecting, target }) => {
+            if (isIntersecting) {
+              target.classList.add("active");
+              sectionsObserver.unobserve(target);
+            }
+          });
         }
-      }
-    });
-  }
+      );
+
+      appSections.forEach(section =>
+        sectionsObserver.observe(section)
+      );
+    } else {
+      appSections.forEach(section => section.classList.add("active"));
+    }
+  });
 })();
