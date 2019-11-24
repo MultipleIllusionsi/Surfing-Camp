01af94ec9dfc05aff2e38c64655859621b36a2bc
MultipleIllusionsi <seregaserega06@gmail.com>
2019-11-24 05:54:49 +0500

    fixed naming in a script and updated index for validation

--- dist/script.js
+++ dist/script.js
@@ -1,24 +1,18 @@
 (function() {
   document.addEventListener("DOMContentLoaded", function() {
-    var appSections = [].slice.call(
-      document.querySelectorAll("section")
-    );
+    const appSections = [].slice.call(document.querySelectorAll("section"));
 
     if ("IntersectionObserver" in window) {
-      let sectionsObserver = new IntersectionObserver(
-        (entries, observer) => {
-          entries.forEach(({ isIntersecting, target }) => {
-            if (isIntersecting) {
-              target.classList.add("active");
-              sectionsObserver.unobserve(target);
-            }
-          });
-        }
-      );
+      let sectionsObserver = new IntersectionObserver((entries, _observer) => {
+        entries.forEach(({ isIntersecting, target }) => {
+          if (isIntersecting) {
+            target.classList.add("active");
+            sectionsObserver.unobserve(target);
+          }
+        });
+      });
 
-      appSections.forEach(section =>
-        sectionsObserver.observe(section)
-      );
+      appSections.forEach(section => sectionsObserver.observe(section));
     } else {
       appSections.forEach(section => section.classList.add("active"));
     }
